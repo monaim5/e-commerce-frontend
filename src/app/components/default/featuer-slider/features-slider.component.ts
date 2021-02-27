3e6509e064279eedfc5677fcc27733d9d71a3892
monaim5 <thexblackcat@gmail.com>
2021-02-27 23:14:41 +0100

    some routine

--- src/app/components/default/featuer-slider/features-slider.component.ts
+++ src/app/components/default/featuer-slider/features-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Promo} from '../../../core/models/promo.model';
 import {PromoService} from '../../../core/services/promo.service';
 import {OwlOptions} from 'ngx-owl-carousel-o';
@@ -12,38 +12,9 @@ export class FeaturesSliderComponent implements OnInit {
 
   promos: Promo[];
   images: any;
+  customOptions: OwlOptions;
+  @ViewChild('carouselContainer') carouselContainer: ElementRef;
 
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navSpeed: 1000,
-    autoplay: true,
-    // mergeFit: true,
-    nav: false,
-    items: 1
-    // navText: ['ssssss', ''],
-    // responsive: {
-    //   0: {
-    //     items: 1
-    //   },
-    //   1100: {
-    //     items: 1
-    //   }
-    // }
-    //   400: {
-    //     items: 1
-    //   },
-    //   740: {
-    //     items: 1
-    //   },
-    //   940: {
-    //     items: 1
-    //   }
-    // }
-  };
 
   constructor(private promoService: PromoService) { }
 
@@ -52,6 +23,64 @@ export class FeaturesSliderComponent implements OnInit {
       this.promos = data;
       this.images = this.promos.map(promo => ({path: promo.banners[0].url}));
     });
+
+    this.customOptions = {
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: true,
+      dots: true,
+      navSpeed: 1000,
+      autoplay: true,
+
+      margin: 10,
+      stagePadding: 0,
+      startPosition: 200,
+
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        600: {
+          items: 2,
+          responsiveRefreshRate: 1
+        },
+        1000: {
+          items: 3,
+          nav: false,
+          loop: false
+        }
+      }
+
+      // nav: false,
+      // items: 1,
+      // margin: this.carouselContainer.nativeElement.offsetWidth
+
+      // navText: ['ssssss', ''],
+      // responsive: {
+      //   0: {
+      //     items: 1
+      //   },
+      //   1100: {
+      //     items: 1
+      //   }
+      // }
+      //   400: {
+      //     items: 1
+      //   },
+      //   740: {
+      //     items: 1
+      //   },
+      //   940: {
+      //     items: 1
+      //   }
+      // }
+    };
+
   }
 
+  hello(event): void {
+    console.log(this.carouselContainer);
+  }
 }
